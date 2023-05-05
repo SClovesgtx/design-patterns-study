638d02c27c600ca17924f2fe498e3b629a29543f
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-05-05 18:56:23 -0300

    testing design observer

--- design_patterns/1_observer/after.py
+++ design_patterns/1_observer/after.py
@@ -31,6 +31,11 @@ class WeatherData:
     __humidity: float
     __pressure: float
 
+    def __init__(self, temperature: float, humidity: float, pressure: float) -> None:
+        self.__temperature = temperature
+        self.__humidity = humidity
+        self.__pressure = pressure
+
     def subscribe(self, subscriber: Display) -> None:
         self.__subscribers.append((subscriber))
         print("Successful subscription!")
@@ -47,14 +52,34 @@ class WeatherData:
         for subscriber in self.__subscribers:
             subscriber.update(self.__temperature, self.__humidity, self.__pressure)
 
-    def __set_temperature(self, temperature: float):
+    def set_temperature(self, temperature: float):
         self.__temperature = temperature
         self.___measurementsChanged()
 
-    def __set_humidity(self, humidity: float):
+    def set_humidity(self, humidity: float):
         self.__humidity = humidity
         self.___measurementsChanged()
 
-    def __set_pressure(self, pressure: float):
+    def set_pressure(self, pressure: float):
         self.__pressure = pressure
         self.___measurementsChanged()
+
+
+if __name__ == "__main__":
+    publisher = WeatherData(23, 92, 1017)
+
+    current_condition = CurrentConditionsDisplay()
+    statistics = StatisticsDisplay()
+    forecast = ForeCastDisplay()
+
+    publisher.subscribe(current_condition)
+    publisher.subscribe(statistics)
+    publisher.subscribe(forecast)
+
+    publisher.set_temperature(22)
+
+    publisher.set_humidity(100)
+
+    publisher.unsubscribe(statistics)
+
+    publisher.set_pressure(1000)
